13d1d1f39adea50a089da2ad30975124c3e6be6b
yangliangfei <yangliangfei@oneband.life>
2018-02-01 23:54:25 +0800

    [UPDATE]增加导航栏组件

---
+++ src/components/Nav.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="nav-main">
+    <div class="left-side">
+      <span>电影点评</span>
+    </div>
+    <div class="right-side">
+      <ul>
+        <router-link v-for="(item, index) in links" :key="index" :to="item.href">
+          {{ item.name }}
+        </router-link>
+        <i class="iconfont icon-search"></i>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data () {
+    return {
+      links: [
+        { name: '发布影评', href: '/' },
+        { name: '精彩影评', href: '/' },
+        { name: '电影排行', href: '/' },
+        { name: '电影推荐', href: '/' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.nav-main {
+  display: flex;
+  padding: 10px 60px;
+  justify-content: space-between;
+  background-color: #0077FF;
+  .left-side {
+    margin: auto 0;
+    color: #FFF;
+    letter-spacing: 0.1px;
+    cursor: pointer;
+  }
+  .right-side {
+    ul {
+      display: flex;
+      a{
+        padding-right: 30px;
+        color: #FFF;
+        letter-spacing: 0.1px;
+        text-decoration: none;
+      }
+      i {
+        margin-top: 3.4px;
+        padding-right: 30px;
+        font-size: 24px;
+        color: #FFF;
+        line-height: 16px;
+        cursor: pointer;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/index/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index-main">
+    <Nav/>
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/Nav'
+
+export default {
+  name: 'Index',
+  components: {
+    Nav
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
