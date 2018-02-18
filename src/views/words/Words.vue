b67ab30028a8264e6f1d29b0a76a5310817644fd
yangliangfei <yangliangfei@oneband.life>
2018-02-18 20:56:50 +0800

    [UPDATE] 增加 ‘ 写台词 ’ 功能

--- src/views/words/Words.vue
+++ src/views/words/Words.vue
@@ -1,32 +1,45 @@
 <template>
   <div class="words-main">
-    <Nav/>
-    <div class="list">
-      <div class="content" v-for="(item, index) in words" :key="index">
-        <div class="paper">
-          <div class="words">
-            <span class="film-words">如果你有梦想，就要守护它。当人们做不到一些事情的时候，他们就会对你说你也同样不可能。有了目标就要全力以赴。</span><br>
-            <span class="film-name">——《当幸福来敲门》</span>
+    <Nav :leftStyle="leftStyle"/>
+    <div class="whole-content">
+      <div class="list">
+        <div class="content" v-for="(item, index) in words" :key="index">
+          <div class="paper">
+            <div class="words">
+              <span class="film-words">如果你有梦想，就要守护它。当人们做不到一些事情的时候，他们就会对你说你也同样不可能。有了目标就要全力以赴。</span><br>
+              <span class="film-name">——《当幸福来敲门》</span>
+            </div>
+            <div class="feature">
+              <div class="unit">
+                <i class="iconfont icon-xihuan"></i>
+                <span>32</span>
+              </div>
+              <div class="unit">
+                <i class="iconfont icon-pinglun"></i>
+                <span>3</span>
+              </div>
+              <div class="unit">
+                <i class="iconfont icon-share"></i>
+              </div>
+            </div>
+            <div class="user-info">
+              <img src="static/images/avatar.jpg" alt="avatar"/>
+              <span>user_name</span>
+            </div>
           </div>
-          <div class="feature">
-            <div class="unit">
-              <i class="iconfont icon-xihuan"></i>
-              <span>32</span>
-            </div>
-            <div class="unit">
-              <i class="iconfont icon-pinglun"></i>
-              <span>3</span>
-            </div>
-            <div class="unit">
-              <i class="iconfont icon-share"></i>
-            </div>
-          </div>
-          <div class="user-info">
-            <img src="static/images/avatar.jpg" alt="avatar"/>
-            <span>user_name</span>
+          <div class="paper-shadow" style="background-image: url(static/images/paper-shadow.png);">
           </div>
         </div>
-        <div class="paper-shadow" style="background-image: url(static/images/paper-shadow.png);">
+      </div>
+      <div class="right-content" v-if="!showContainer">
+        <button @click="addWords">写台词</button>
+      </div>
+      <div class="container" v-else>
+        <textarea class="words-area" placeholder="请输入台词"></textarea>
+        <textarea class="name-area" placeholder="请输入电影名"></textarea>
+        <div class="buttons">
+          <button class="cancel" @click="cancel">取消</button>
+          <button class="sure">确认</button>
         </div>
       </div>
     </div>
@@ -43,11 +56,24 @@ export default {
   },
   data () {
     return {
-      words: []
+      leftStyle: 'margin-left: 260px;',
+      words: [],
+      showContainer: false
     }
   },
   created () {
     this.words.length = 6
+  },
+  methods: {
+    addWords () {
+      this.showContainer = true
+      this.leftStyle = 'margin-left: 34px;'
+      setTimeout(scrollTo(0, 0), 100)
+    },
+    cancel () {
+      this.showContainer = false
+      this.leftStyle = 'margin-left: 260px;'
+    }
   }
 }
 </script>
@@ -56,56 +82,135 @@ export default {
 .words-main {
   height: 100%;
   background-color: #F6F6F6;
-  .list {
-    margin: 48px 0;
-    .content {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      .paper {
-        padding: 32px;
-        padding-bottom: 12px;
-        width: 736px;
-        background-color: #FFF;
-        border: 1px solid #BDBDBD;
-        .words {
-          padding-bottom: 28px;
-          border-bottom: 1px solid #BDBDBD;
-          .film-words {
-            font-size: 18px;
-            font-weight: 600;
+  .whole-content {
+    display: flex;
+    justify-content: center;
+    .list {
+      margin: 48px 0;
+      .content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .paper {
+          padding: 32px;
+          padding-bottom: 12px;
+          width: 736px;
+          background-color: #FFF;
+          border: 1px solid #BDBDBD;
+          .words {
+            padding-bottom: 28px;
+            border-bottom: 1px solid #BDBDBD;
+            .film-words {
+              font-size: 18px;
+              font-weight: 600;
+            }
+            .film-name {
+              float: right;
+            }
           }
-          .film-name {
-            float: right;
-          }
-        }
-        .feature {
-          margin-top: 10px;
-          .unit {
+          .feature {
             margin-top: 10px;
-            margin-right: 16px;
-            float: left;
-            cursor: pointer;
+            .unit {
+              margin-top: 10px;
+              margin-right: 16px;
+              float: left;
+              cursor: pointer;
+            }
+          }
+          .user-info {
+            display: flex;
+            float: right;
+            align-items: center;
+            img {
+              margin-right: 8px;
+              width: 42px;
+              height: 42px;
+              border-radius: 50%;
+            }
+            img, span {
+              cursor: pointer;
+            }
           }
         }
-        .user-info {
-          display: flex;
-          float: right;
-          align-items: center;
-          img {
-            margin-right: 8px;
-            width: 42px;
-            height: 42px;
-            border-radius: 50%;
-          }
-          img, span {
-            cursor: pointer;
-          }
+        .paper-shadow {
+          width: 800px;
+          height: 32px;
         }
       }
-      .paper-shadow {
-        width: 800px;
+    }
+    .right-content {
+      position: fixed;
+      top: 180px;
+      right: 200px;
+      button {
+        width: 80px;
         height: 32px;
+
+        color: #0077FF;
+        background-color: #FFF;
+
+        font-size: 14px;
+        font-weight: 600;
+        letter-spacing: 1px;
+
+        border: 1px solid #BDBDBD;
+        border-radius: 3px;
+        outline: none;
+        cursor: pointer;
+
+        &:hover {
+          box-shadow: 0 0 2px;
+        }
+      }
+    }
+    .container {
+      display: flex;
+      margin: 48px;
+      padding: 20px;
+      height: 100%;
+      flex-direction: column;
+      background-color: #FFF;
+      border: 1px solid #BDBDBD;
+      border-radius: 3px;
+      .words-area {
+        width: 300px;
+        height: 210px;
+        font-size: 18px;
+        line-height: 24px;
+      }
+      .name-area {
+        margin: 20px 0;
+        height: 20px;
+        font-size: 14px;
+      }
+      textarea {
+        padding: 8px;
+        border: 1px solid #BDBDBD;
+        border-radius: 3px;
+        resize: none;
+        outline: none;
+      }
+      .buttons {
+        display: flex;
+        justify-content: space-between;
+        .cancel {
+          color: gray;
+        }
+        .sure {
+          color: #FFF;
+          background-color: #0077FF;
+          border: none;
+        }
+        button {
+          width: 120px;
+          height: 30px;
+          font-size: 14px;
+          letter-spacing: 1px;
+          border: 1px solid #BDBDBD;
+          border-radius: 3px;
+          outline: none;
+          cursor: pointer;
+        }
       }
     }
   }
