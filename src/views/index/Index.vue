e4e6067c6a3546f3874eb5facfa08de499f7530e
yangliangfei <yangliangfei@oneband.life>
2018-03-12 23:38:28 +0800

    [UPDATE] 首页改版

--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -1,49 +1,49 @@
 <template>
   <div class="index-main">
-    <Nav :leftStyle="leftStyle"/>
-    <div class="slider">
-      <div class="control">
-        <i class="iconfont icon-iconfontzhizuobiaozhun023126" @click="replace(index - 1)"></i>
-        <i class="iconfont right icon-iconfontzhizuobiaozhun023127" @click="replace(index + 1)"></i>
-      </div>
-      <div class="list" ref="list">
-        <img src="static/images/fox.jpg" alt="image"/>
-        <img src="static/images/orgx.jpg" alt="image"/>
-        <img src="static/images/tiger.jpg" alt="image"/>
-      </div>
-    </div>
-    <div class="pointer">
-      <li v-for="i in 3" :key="i" ref="pointer" @click="replace(index)"></li>
-      <!-- <span style="color: red;">轮播图上加说明文字</span> -->
-    </div>
-    <div class="content">
-      <div class="left-content">
-        <div class="review" v-for="i in 6" :key="i" @click="jump">
-          <img src="static/images/bg.jpg" alt="image"/>
-          <div class="title">
-            <span class="deck"></span>
-            <span>标题标题标题标题标题</span>
-          </div>
-        </div>
-      </div>
-      <div class="right-content">
-        <!-- 设计一个样式，冠军、亚军、季军 -->
-        <div class="tip">
-          <span>电影排行榜</span>
-          <router-link to="/rank">查看更多</router-link>
-        </div>
-        <div class="classic" v-for="i in 3" :key="i">
-          <div class="top">
-            <span>{{ i }}.</span>
-            <img src="https://img1.doubanio.com/view/photo/s_ratio_poster/public/p1312700628.jpg" alt="image"/>
-            <div class="film-info">
-              <span>当幸福来敲门</span>
-              <span>信息简介</span>
+    <Nav/>
+    <div class="left">
+      <div class="content" v-for="i in 10" :key="i">
+        <div class="top">
+          <img src="https://img3.doubanio.com/view/photo/s_ratio_poster/public/p457760035.jpg" alt="poster">
+          <div class="right-content">
+            <div class="info">
+              <img src="static/images/avatar.jpg" alt="avatar">
+              <span>用户昵称</span>
+              <span>评分</span>
+              <span>2018-03-12 14:00</span>
+            </div>
+            <span>标题</span>
+            <div class="preview">
+              <span>内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容</span>
             </div>
           </div>
-          <!-- 加上双引号背景 -->
-          <div class="bottom">
-            <span>一句短评</span>
+        </div>
+        <div class="feature">
+          <li v-for="i in 3" :key="i">功能{{ i }}</li>
+        </div>
+      </div>
+    </div>
+    <div class="right">
+      <div class="sidebar">
+        <div class="type" v-for="i in 9" :key="i">
+          <span>类型</span>
+        </div>
+      </div>
+      <div class="rank">
+        <div class="header">
+          <span>电影排行</span>
+          <div class="random">
+            <i class="iconfont icon-icon--"></i>
+            <span>换一换</span>
+          </div>
+        </div>
+        <div class="recommend" v-for="i in 3" :key="i">
+          <span>{{ i }}.</span>
+          <img src="https://img3.doubanio.com/view/photo/s_ratio_poster/public/p480747492.jpg" alt="poster">
+          <div class="info">
+            <span>当幸福来敲门</span>
+            <span>信息简介</span>
+            <span>评分及星标</span>
           </div>
         </div>
       </div>
@@ -52,182 +52,137 @@
 </template>
 
 <script>
-import Nav from '@/components/Nav'
+import Nav from '@/components/Nav';
 
 export default {
   name: 'Index',
   components: {
-    Nav
+    Nav,
   },
-  data () {
-    return {
-      leftStyle: 'margin-left: -8px;',
-      index: 0
-    }
-  },
-  methods: {
-    replace (index) {
-      this.$refs.pointer[this.index].style.color = 'gray'
-      this.index = index
-      if (index > 2) {
-        this.index = 0
-      } else if (index < 0) {
-        this.index = 2
-      }
-      this.$refs.pointer[this.index].style.color = 'orange'
-      this.$refs.list.style.left = -(this.index * 100) + 'vw'
-    },
-    jump () {
-      this.$router.push('/review')
-    }
-  }
 }
 </script>
 
 <style lang="less" scoped>
 .index-main {
   background-color: #F6F6F6;
-  .slider {
-    position: relative;
-    width: 100vw;
-    height: 50vh;
-    overflow: hidden;
-    .control {
-      display: none;
-      position: absolute;
-      top: 173px;
-      left: 60px;
-      z-index: 2;
-      i {
-        color: white;
-        font-size: 36px;
-        cursor: pointer;
-        &:hover {
-          color: orange;
+  .left {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 80%;
+    .content {
+      display: flex;
+      margin-top: 20px;
+      padding: 12px;
+      width: 60%;
+      flex-direction: column;
+      background-color: #FFF;
+      box-shadow: 0 1px 3px #BDBDBD;
+      border-radius: 3px;
+      .top {
+        display: flex;
+        img {
+          align-self: center;
+          width: 75px;
+          height: 108px;
+          border-radius: 3px;
+        }
+        .right-content {
+          margin-left: 14px;
+          img {
+            width: 32px;
+            height: 32px;
+            border-radius: 50%;
+          }
+          .info {
+            display: flex;
+            align-items: center;
+            span {
+              margin-left: 8px;
+            }
+          }
+          .preview {
+            display: -webkit-box;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            -webkit-line-clamp: 3;
+            -webkit-box-orient: vertical;
+          }
         }
       }
-      .right {
-        margin-left: 86vw;
-      }
-    }
-    &:hover {
-      .control {
-        display: block;
-      }
-    }
-    .list {
-      display: flex;
-      position: absolute;
-      width: 300vw;
-      height: 60vh;
-      cursor: pointer;
-      img {
-        width: 100vw;
-        height: 50vh;
+      .feature {
+        margin-top: 12px;
+        li {
+          margin-right: 12px;
+          float: left;
+          list-style: none;
+        }
       }
     }
   }
-  .pointer {
-    display: flex;
-    margin: 26px 0 10px 0;
-    justify-content: center;
-
-    color: gray;
-    font-size: 20px;
-    line-height: 20px;
-    li {
-      margin-right: 4px;
-      cursor: pointer;
-      &:first-child {
-        color: orange;
-      }
-    }
-  }
-  .content {
-    display: flex;
-    margin: 0 auto;
-    justify-content: center;
-    .left-content {
+  .right {
+    position: fixed;
+    top: 82px;
+    right: 10%;
+    .sidebar {
       display: flex;
       flex-wrap: wrap;
-      margin: 16px 0;
-      width: 75%;
-      .review {
-        margin-right: 20px;
-        img {
-          width: 340px;
-          height: 220px;
-          border-radius: 3px;
-          box-shadow: 0 1px 3px rgba(26, 26, 26, .6);
-          cursor: pointer;
-        }
-        .title {
-          position: relative;
-          bottom: 50px;
-          width: 100%;
-          height: 45px;
-          line-height: 45px;
-
-          text-align: left;
-          letter-spacing: 1px;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          white-space: nowrap;
-
-          color: #FFF;
-          background-color: rgba(128, 128, 128, .66);
-          border-bottom-left-radius: 3px;
-          border-bottom-right-radius: 3px;
-          cursor: pointer;
-          .deck {
-            position: relative;
-            margin-right: 8px;
-            width: 10px;
-            height: 16px;
-            border-left: 4px solid #8692A6;
-          }
+      justify-content: center;
+      width: 280px;
+      // height: 360px;
+      background-color: #FFF;
+      box-shadow: 0 1px 3px #BDBDBD;
+      border-radius: 3px;
+      .type {
+        display: flex;
+        margin: 12px 0 0 12px;
+        width: 68px;
+        height: 68px;
+        justify-content: center;
+        align-items: center;
+        color: gray;
+        letter-spacing: 1px;
+        box-shadow: 0 1px 3px #BDBDBD;
+        border-radius: 3px;
+        cursor: pointer;
+        &:last-child {
+          margin-bottom: 12px;
         }
       }
     }
-    .right-content {
-      margin: 16px 0 0 8px;
-      padding: 10px;
+    .rank {
+      margin-top: 20px;
       height: 100%;
+      width: 100%;
       background-color: #FFF;
+      box-shadow: 0 1px 3px #BDBDBD;
       border-radius: 3px;
-      box-shadow: 0 1px 3px rgba(26, 26, 26, .6);
-      .tip {
+      .header {
         display: flex;
         justify-content: space-between;
-        span {
-          font-weight: 600;
-        }
-        a {
-          color: #175199;
+        padding: 12px;
+        padding-bottom: 0;
+        .random {
           font-size: 13px;
-          text-decoration: none;
+          color: #175199;
+          cursor: pointer;
+          i {
+            font-weight: 600;
+          }
         }
       }
-      .classic {
-        margin-top: 10px;
-        .top {
-          display: flex;
-          img {
-            margin-left: 3px;
-            width: 100px;
-            height: 120px;
-          }
-          .film-info {
-            display: flex;
-            padding: 8px;
-            padding-top: 0;
-            width: 100%;
-            flex-direction: column;
-            align-items: center;
-          }
+      .recommend {
+        display: flex;
+        padding: 12px;
+        img {
+          margin: 0 12px 0 8px;
+          width: 75px;
+          height: 108px;
+          border-radius: 3px;
         }
-        .bottom {
-          margin-top: 6px;
+        .info {
+          display: flex;
+          flex-direction: column;
         }
       }
     }
