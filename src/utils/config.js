2d726e98de853a1c2e8884ab7419f6d3f2295465
yangliangfei <yangliangfei@oneband.life>
2018-03-18 15:08:26 +0800

    [FIX]

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,10 @@
-// const isDev = ['localhost', '43.251.100.34'].includes(location.hostname);
+const isDev = ['localhost', '43.251.100.34'].includes(location.hostname);
 
-const isDev = ['localhost'].includes(location.hostname);
+// const isDev = ['localhost'].includes(location.hostname);
 
 // const HOST = isDev ? 'http://localhost:3000' : 'http://43.251.100.34:3000';
 
-const HOST = 'http://localhost:3000';
+const HOST = 'http://127.0.0.1:3000';
 // const HOST = 'http://43.251.100.34:3000';
 
 export { isDev, HOST }
