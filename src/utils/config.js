0aeafe9c00eaaf35f23df043dcc98fd9e12ee5f3
yangliangfei <yangliangfei@oneband.life>
2018-03-24 17:50:02 +0800

    try to fix

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,8 +4,6 @@ const isDev = ['localhost', '43.251.100.34'].includes(location.hostname);
 
 // const HOST = isDev ? 'http://localhost:3000' : 'http://43.251.100.34:3000';
 
-// const HOST = 'http://43.251.100.34:3000';
-const HOST = '';
-// const HOST = 'http://43.251.100.34:3000';
+const HOST = 'http://43.251.100.34:3000';
 
 export { isDev, HOST }
