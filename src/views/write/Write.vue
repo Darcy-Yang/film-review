aea6f4533d54e223ea14d256c1e8bf6a8df316d9
yangliangfei <yangliangfei@oneband.life>
2018-03-27 23:18:35 +0800

    [UPDATE] 更新 ‘发布影评’ 页面

--- src/views/write/Write.vue
+++ src/views/write/Write.vue
@@ -2,10 +2,29 @@
   <div class="write-main">
     <Nav/>
     <div class="area">
-      <div class="upload-area">
-        <i class="iconfont icon-xiangji"></i>
-        <span class="description">添加背景图</span>
-        <input type="file"/>
+      <div class="info-area">
+        <div class="select-area">
+          <input type="text" v-model="searchWord"/>
+          <ul v-show="showSelect">
+            <li v-for="(movie, index) in ranks" :key="index" @click="choose(movie)">
+              {{ movie.title }}
+            </li>
+          </ul>
+        </div>
+        <div class="movie-info" v-if="!movie">
+          <h2>电影信息</h2>
+        </div>
+        <div class="movie-info" v-else>
+          <div class="poster">
+            <img class="poster" :src="movie.img_src" alt="poster"/>
+            <span>{{ movie.quote }}</span>
+          </div>
+          <div class="info">
+            <span>{{ movie.title }}</span>
+            <span>{{ movie.info }}</span>
+            <span>{{ movie.movie_type }}</span>
+          </div>
+        </div>
       </div>
       <div class="title-area">
         <textarea placeholder="请输入标题"></textarea>
@@ -20,7 +39,9 @@
 </template>
 
 <script>
-import Nav from '@/components/Nav'
+import Nav from '@/components/Nav';
+
+import request from '@/utils/request';
 
 export default {
   name: 'Write',
@@ -29,11 +50,42 @@ export default {
   },
   data () {
     return {
-      contents: ''
+      contents: '',
+      searchWord: '',
+      ranks: [],
+      movie: null,
+      showSelect: false,
+      closeSelect: false,
     }
   },
   methods: {
-  }
+    choose(movie) {
+      this.searchWord = movie.title;
+      this.movie = movie;
+      this.closeSelect = true;
+    },
+  },
+  watch: {
+    async searchWord(val) {
+      if (this.closeSelect) {
+        this.showSelect = false;
+        this.closeSelect = false;
+        return;
+      }
+      this.showSelect = !!val;
+      if (val) {
+        try {
+          const { ranks } = await request('POST', '/rank', { searchWord: this.searchWord });
+          this.ranks = ranks;
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+        this.ranks = null;
+        this.movie = null;
+      }
+    }
+  },
 }
 </script>
 
@@ -52,42 +104,72 @@ export default {
       border: none;
       resize: none;
     }
-    .upload-area {
+    .info-area {
       display: flex;
-      position: relative;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-
-      // width: 42%;
       width: 100%;
-      height: 192px;
-
-      color: gray;
-      background-color: #F7F8F9;
-      border-radius: 3px;
-      i {
-        font-size: 32px;
-      }
-      .description {
-        display: none;
-      }
-      &:hover {
-        .description {
-          display: block;
+      min-height: 200px;
+      .select-area {
+        position: relative;
+        flex-grow: 1;
+        input {
+          width: 80%;
+          padding: 0 0 4px 12px;
+          font-size: 16px;
+          outline: none;
+          border: none;
+          border-bottom: 1px solid #BDBDBD;
+        }
+        ul {
           position: absolute;
-          top: 120px;
+          margin-top: 12px;
+          padding: 6px 12px 6px 0;
+          width: 80%;
+
+          background-color: #FFF;
+          border-radius: 4px;
+          box-shadow: 0 1px 3px rgba(26, 26, 26, .3);
+          z-index: 1;
+          li {
+            width: 100%;
+            margin: 4px 0;
+            padding: 4px 0 4px 12px;
+            list-style: none;
+
+            transition: .15s;
+            cursor: pointer;
+            &:hover {
+              color: #0077FF;
+              background-color: rgba(26, 26, 26, .1);
+            }
+          }
         }
       }
-      input {
-        position: absolute;
-        top: 0;
-        left: 0;
-        height: 100%;
-        width: 100%;
+      .movie-info {
+        flex-grow: 1;
+        display: flex;
+        max-width: 50%;
+        .poster {
+          margin-right: 20px;
+          width: 136px;
+          height: 190px;
 
-        opacity: 0;
-        cursor: pointer;
+          border-radius: 4px;
+          box-shadow: 0 1px 2px rgba(26, 26, 26, .3);
+          span {
+            padding-left: 6px;
+            font-style: italic;
+          }
+        }
+        .info {
+          display: flex;
+          flex-direction: column;
+          span {
+            margin-bottom: 8px;
+            &:first-child {
+              font-size: 18px;
+            }
+          }
+        }
       }
     }
     .title-area {
@@ -97,8 +179,9 @@ export default {
       textarea {
         padding: 0;
         height: 44px;
-        font-size: 32px;
         line-height: 1.4;
+
+        font-size: 32px;
         font-weight: 600;
         letter-spacing: 1px;
       }
