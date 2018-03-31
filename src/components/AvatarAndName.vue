1b69db5f11e9755d4f7997d32826320f63af70c8
yangliangfei <yangliangfei@oneband.life>
2018-03-31 21:54:23 +0800

    [UPDATE] 增加 ‘头像及姓名’ 组件

---
+++ src/components/AvatarAndName.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="avatar-name-main">
+    <div class="avatar-name">
+      <img src="static/images/avatar.jpg" alt="avatar"/>
+      <span>name</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AvatarAndName'
+}
+</script>
+
+<style lang="less" scoped>
+.avatar-name-main {
+  .avatar-name {
+    display: flex;
+    align-items: center;
+    img {
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      margin-right: 8px;
+    }
+    img, span {
+      cursor: pointer;
+    }
+  }
+}
+</style>
