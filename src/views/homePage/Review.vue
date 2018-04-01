b5fc952dc13a832796a14003ed4f09cf44da6591
yangliangfei <yangliangfei@oneband.life>
2018-04-01 21:45:36 +0800

    [UPDATE] 个人中心增加影评数据

--- src/views/homePage/Review.vue
+++ src/views/homePage/Review.vue
@@ -4,13 +4,45 @@
       <div class="explain">
         <span>我的影评</span>
       </div>
+      <div v-for="(review, index) in reviews" :key="index">
+        <div class="info">
+          <span>{{ review.title }}</span>
+        </div>
+        <div class="situation">
+          <span>{{ review.createdAt }}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import request from '@/utils/request';
+import { getUser } from '@/utils/user';
+
 export default {
-  name: 'Review'
+  name: 'Review',
+  data() {
+    return {
+      uesr: null,
+      reviews: [],
+    }
+  },
+  created() {
+    const { user } =getUser();
+    this.user = user;
+    this.getReviews(this.user.id)
+  },
+  methods: {
+    async getReviews(id) {
+      try {
+        const { reviews } = await request('GET', `/review/${id}/all`);
+        this.reviews = reviews;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
 }
 </script>
 <style lang="less" scoped>
