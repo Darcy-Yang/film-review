8993d2878e7b77c361c7b2881201fa051cb4c626
yangliangfei <yangliangfei@oneband.life>
2018-04-01 21:46:10 +0800

    [UPDATE] 个人中心增加点赞影评数据

--- src/views/homePage/Like.vue
+++ src/views/homePage/Like.vue
@@ -4,13 +4,43 @@
       <div class="explain">
         <span>我喜欢的</span>
       </div>
+      <div v-for="(like, index) in likes" :key="index">
+        <div class="info">
+          <span>{{ like.review.title }}</span>
+        </div>
+        <div class="situation">
+          <span>{{ like.createdAt }}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import request from '@/utils/request';
+import { getUser } from '@/utils/user';
+
 export default {
-  name: 'Like'
+  name: 'Like',
+  data() {
+    return {
+      likes: [],
+    }
+  },
+  created() {
+    const { user } = getUser();
+    this.getLikes(user.id);
+  },
+  methods: {
+    async getLikes(id) {
+      try {
+        const { likes } = await request('GET', `/like/${id}/all`);
+        this.likes = likes;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
 }
 </script>
 <style lang="less" scoped>
